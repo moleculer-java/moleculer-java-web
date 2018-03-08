543aad51ef8b0e6527816f6e3368f4a1d19d2e54
Berkes Andras <andras.berkes@mail.com>
2018-03-08 10:59:01 +0100

    First commit

---
+++ src/test/java/services/moleculer/web/Sample.java
@@ -0,0 +1,115 @@
+package services.moleculer.web;
+
+import io.datatree.Tree;
+import services.moleculer.ServiceBroker;
+import services.moleculer.config.ServiceBrokerConfig;
+import services.moleculer.context.CallingOptions;
+import services.moleculer.context.Context;
+import services.moleculer.eventbus.Listener;
+import services.moleculer.eventbus.Subscribe;
+import services.moleculer.repl.LocalRepl;
+import services.moleculer.service.Action;
+import services.moleculer.service.Middleware;
+import services.moleculer.service.Name;
+import services.moleculer.service.Service;
+import services.moleculer.service.Version;
+import services.moleculer.transporter.RedisTransporter;
+import services.moleculer.web.middleware.CorsHeaders;
+import services.moleculer.web.router.Alias;
+import services.moleculer.web.router.MappingPolicy;
+import services.moleculer.web.router.Route;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("START");
+		try {
+			ServiceBrokerConfig cfg = new ServiceBrokerConfig();
+			
+			RedisTransporter t = new RedisTransporter();
+			t.setDebug(false);
+			// cfg.setTransporter(t);
+			
+			cfg.setRepl(new LocalRepl());
+			
+			NettyGateway gateway = new NettyGateway();
+			gateway.setUseSSL(false);
+			gateway.setKeyStoreFilePath("/temp/test.jks");
+			gateway.setKeyStorePassword("test");
+			cfg.setApiGateway(gateway);
+			
+			ServiceBroker broker = new ServiceBroker(cfg);
+
+			String path = "/math";
+			MappingPolicy policy = MappingPolicy.ALL;
+			CallingOptions.Options opts = CallingOptions.retryCount(3);
+			String[] whitelist = {};
+			Alias[] aliases = new Alias[1];
+			aliases[0] = new Alias(Alias.ALL, "/add", "math.add");
+			
+			Route r = new Route(gateway, path, policy, opts, whitelist, aliases);
+			r.use(new CorsHeaders());
+			gateway.setRoutes(new Route[]{r});
+
+			// gateway.use(new ServeStatic("/pages", "c:/temp"));
+			// gateway.use(new SessionCookie());
+			// gateway.use(new RequestLogger());
+		
+			broker.createService(new Service("math") {
+
+				@Name("add")
+				//@Cache(keys = { "a", "b" }, ttl = 30)
+				public Action add = ctx -> {
+
+					//broker.getLogger().info("Call " + ctx.params);
+					return ctx.params.get("a", 0) + ctx.params.get("b", 0);
+
+				};
+
+				@Name("test")
+				@Version("1")
+				public Action test = ctx -> {
+
+					return ctx.params.get("a", 0) + ctx.params.get("b", 0);
+
+				};
+
+				@Subscribe("foo.*")
+				public Listener listener = payload -> {
+					System.out.println("Received: " + payload);
+				};
+
+			});			
+			broker.start();
+			broker.use(new Middleware() {
+
+				@Override
+				public Action install(Action action, Tree config) {
+					int version = config.get("version", 0);
+					if (version > 0) {
+						broker.getLogger().info("Middleware installed to " + config.toString(false));
+						return new Action() {
+
+							@Override
+							public Object handler(Context ctx) throws Exception {
+								Object original = action.handler(ctx);
+								Object replaced = System.currentTimeMillis();
+								broker.getLogger()
+										.info("Middleware invoked! Replacing " + original + " to " + replaced);
+								return replaced;
+							}
+
+						};
+					}
+					broker.getLogger().info("Middleware not installed to " + config.toString(false));
+					return null;
+				}
+
+			});
+
+		} catch (Exception cause) {
+			cause.printStackTrace();
+		}
+	}
+
+}
